11bef1814caa16dc4e68c933879da55a39f2c7dd
Michael <osujim2012@gmail.com>
2023-04-12 14:01:02 -0400

    Create videos.php

---
+++ videos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>445 Lab 2 Client</title>
+    <link rel="stylesheet" href="./css/styles.css" />
+  </head>
+  <body>
+    <div id="container">
+      <h2>UPLOADED VIDEOS</h2>
+      <ul>
+        <?php
+          $video_directory = dirname(__FILE__) . '/api/videos/';
+          $videos = glob($video_directory . '*.mp4');
+          foreach($videos as $video) {
+            echo '<li>' . basename($video) . '</li>';
+          }
+        ?>
+      </ul>
+    </div>
+    <script type="module" src="./js/main.js" defer></script>
+  </body>
+</html>
